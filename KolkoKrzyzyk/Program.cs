d877c8ee9c2b00941aa3e0ec117b1289c147cfbe
mateusz.marzec <mateusz_m98@wp.pl>
2019-04-14 12:09:30 +0200

    test

--- KolkoKrzyzyk/Program.cs
+++ KolkoKrzyzyk/Program.cs
@@ -26,7 +26,7 @@ namespace KolkoKrzyzyk
             Console.WriteLine("Wpisz imie gracza B: ");
             imieGraczaB = Console.ReadLine();
 
-
+            /*******************/
 
 
 
--- zadanie_2_3/Program.cs
+++ zadanie_2_3/Program.cs
@@ -10,6 +10,20 @@ namespace zadanie_2_3
     {
         static void Main(string[] args)
         {
+            int a = 3, b = 4;
+            Console.WriteLine("Przed zmianą: a = {0}, b = {1}", a, b);
+
+            Interchange(ref a, ref b);
+            Console.WriteLine("Po zmianie:   a = {0}, b = {1}", a, b);
+
+            Console.ReadKey();
+        }
+
+        static void Interchange(ref int aa, ref int bb)
+        {
+            int tmp = aa;
+            aa = bb;
+            bb = tmp;
         }
     }
 }
