a30d83340cad6fb3e71ba715820b672051449cc4
mateusz.marzec <mateusz_m98@wp.pl>
2019-04-14 11:37:03 +0200

    First versio of the code

--- KolkoKrzyzyk/Program.cs
+++ KolkoKrzyzyk/Program.cs
@@ -10,6 +10,31 @@ namespace KolkoKrzyzyk
     {
         static void Main(string[] args)
         {
+            string imieGraczaA = "";
+            string imieGraczaB = "";
+            char znakGraczaA = 'x';
+            char znakGraczaB = 'o';
+            char[,] plansza = new char[3, 3] {
+                { '1', '2', '3' },
+                { '4', '5', '6' },
+                { '7', '8', '9' }
+
+            };
+
+            Console.WriteLine("Wpisz imie gracza A: ");
+            imieGraczaA = Console.ReadLine();
+            Console.WriteLine("Wpisz imie gracza B: ");
+            imieGraczaB = Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
