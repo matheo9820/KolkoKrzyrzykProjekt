644c5b463d8742c7645f35c375e0cc00c2dd8d9f
mateusz.marzec <mateusz_m98@wp.pl>
2019-06-02 12:46:26 +0200

    Add game over checking

--- KolkoKrzyzyk/Program.cs
+++ KolkoKrzyzyk/Program.cs
@@ -23,11 +23,12 @@ namespace KolkoKrzyzyk
                 { '7', '8', '9' }
 
             };
+            char[,] planszaKopia = plansza.Clone() as char[,];
                 
 
             bool koniecGry = false;
             bool ruchGraczaA = true;
-            while (!koniecGry)
+            for (int runda = 0; runda < plansza.Length; ++runda)
             {
                 Console.Clear();
                 RysujPlansze(plansza);
@@ -35,13 +36,13 @@ namespace KolkoKrzyzyk
                 if (ruchGraczaA)
                 {
                     Console.WriteLine("Ruch wykonuje: " + gA.Imie);
-                    koniecGry = gA.WykonajRuch(plansza);
+                    koniecGry = gA.WykonajRuch(plansza, planszaKopia);
                     ruchGraczaA = false;
                 }
                 else
                 {
                     Console.WriteLine("Ruch wykonuje: " + gB.Imie);
-                    koniecGry = gA.WykonajRuch(plansza);
+                    koniecGry = gB.WykonajRuch(plansza, planszaKopia);
                     ruchGraczaA = true;
                 }
 
@@ -61,7 +62,8 @@ namespace KolkoKrzyzyk
 
             for (int i = 0; i < wysokosc; ++j)
             {
-                Console.Write(plansza[i, j]);
+                for (int j = 0; i < wysokosc; ++i)
+                    Console.Write(plansza[i, j]);
                 Console.WriteLine();
             }
         }
@@ -69,30 +71,81 @@ namespace KolkoKrzyzyk
 
     interface IRuch
     {
-        bool WykonajRuch(char[,] plansza);    
+        bool WykonajRuch(char[,] plansza, char[,] planszaKopia);    
     }
 
     abstract class Gracz
     {
         public string Imie {get; set; }
         public char Znak {get; set; }
+
+        public bool SprawdzCzyKoniecGry(char[,] plansza)
+        {
+            int wysokosc = plansza.GetLength(0);
+            int szerokosc = plansza.GetLength(1);
+            if (szerokosc != wysokosc)
+                throw new Exception("Plansza nie jest kwadratowa!");
+
+            // Sprawdz wiersze
+            for (int i = 0; i < wysokosc; ++i)
+            {
+                int sumaWiersza = 0;
+                for (int j = 0; j < szerokosc; ++j)
+                {
+                    if (plansza[i, j] == Znak)
+                        ++sumaWiersza;
+                }
+                if (sumaWiersza == szerokosc)
+                    return true;
+            }
+
+            // Sprawdz kolumny
+            for (int j = 0; j < wysokosc; ++j)
+            {
+                int sumaKolumny = 0;
+                for (int i = 0; i < szerokosc; ++i)
+                {
+                    if (plansza[i, j] == Znak)
+                        ++sumaKolumny;
+                }
+                if (sumaKolumny == wysokosc)
+                    return true;
+            }
+
+            // Sprawdz przekatne
+            int sumaPrzekatnejA = 0;
+            int sumaPrzekatnejB = 0;
+            for (int k = 0; k < szerokosc; ++k)
+            {
+                if (plansza[k, k] == Znak)
+                    ++sumaPrzekatnejA;
+                if (plansza[k, szerokosc - 1 - k] == Znak)
+                    ++sumaPrzekatnejB;
+            }
+            if (sumaPrzekatnejA == szerokosc || sumaPrzekatnejB == szerokosc)
+                return true;
+
+            return false;
+        }
     }
 
     class GraczLudzki : Gracz, IRuch
     {
-        public bool WykonajRuch(char[,] plansza)
+        public bool WykonajRuch(char[,] plansza, char [,] planszaKopia)
         {
-            return false;
+            return SprawdzCzyKoniecGry(plansza);
         }
     }
 
     class GraczKomputerowy : Gracz, IRuch
     {
-        public bool WykonajRuch(char[,] plansza)
+        public bool WykonajRuch(char[,] plansza, char[,] planszaKopia)
         {
-            return false;
+            return SprawdzCzyKoniecGry(plansza);
         }       
     }
+
+   
 
 
 
